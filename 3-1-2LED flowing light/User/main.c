ca12962847d2ead7c1f2950b3455eac3c689d727
Luoka666 <3266380141@qq.com>
2026-02-24 19:24:05 +0800

    对闪烁代码进行了重构

--- 3-1-2LED flowing light/User/main.c
+++ 3-1-2LED flowing light/User/main.c
@@ -9,18 +9,40 @@ int main(void){
 	P.GPIO_Speed =  GPIO_Speed_50MHz;
 	GPIO_Init(GPIOA,&P);
 	uint16_t pins[] = {GPIO_Pin_0,GPIO_Pin_1,GPIO_Pin_2,GPIO_Pin_3,GPIO_Pin_4,GPIO_Pin_5,GPIO_Pin_6,GPIO_Pin_7};
-	while(1){
-		for(int i  = 0; i < 8; i++){
-			
-			//关闭前一个灯
-			GPIO_ResetBits(GPIOA,pins[i - 1]);
-			//打开LED
-			GPIO_SetBits(GPIOA,pins[i]);
+	while(1){ 
+		//用GPIO_ResetBits && GPIO_SetBits
+//		for(int i  = 0; i < 8; i++){
+//			
+//			//关闭前一个灯
+//			GPIO_ResetBits(GPIOA,pins[i - 1]);
+//			//打开LED
+//			GPIO_SetBits(GPIOA,pins[i]);
+//			Delay_ms(200);
+//			if( i == 7)//最后循环到7时需要在跳出for循环之前置GPIO_Pin_7为0，关闭led灯
+//				GPIO_ResetBits(GPIOA,pins[i]);
+//			
+//		}
+		//用GPIO_WriteBit
+//		for(int i = 0; i < 8; i++){
+//			
+//			GPIO_WriteBit(GPIOA,pins[i - 1],Bit_RESET);
+//			GPIO_WriteBit(GPIOA,pins[i],Bit_SET);
+//			Delay_ms(200);
+//			if(i == 7)
+//				GPIO_WriteBit(GPIOA,pins[i],Bit_RESET);
+//		
+//		}
+		for(int i = 0; i < 8; i++) {
+			// 1. 点亮当前引脚
+			GPIO_WriteBit(GPIOA, pins[i], Bit_SET);
 			Delay_ms(200);
-			if( i == 7)
-				GPIO_ResetBits(GPIOA,pins[i]);
 			
+			// 2. 熄灭当前引脚（为下一次循环点亮下一个引脚做准备）
+			GPIO_WriteBit(GPIOA, pins[i], Bit_RESET);
+			
+			// 这种写法简单清晰：亮 -> 延时 -> 灭 -> 循环下一个
 		}
+		
 		
 //		GPIO_ResetBits(GPIOA,GPIO_Pin_7);
 //		GPIO_SetBits(GPIOA,GPIO_Pin_0);
