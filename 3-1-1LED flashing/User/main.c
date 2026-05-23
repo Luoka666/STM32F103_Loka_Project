e3d254f0a1408514df696e3a0465fc0f0b8f4a8a
李佳乐 <3266380141@qq.com>
2026-05-23 23:46:15 +0800

    小改动

--- 3-1-1LED flashing/User/main.c
+++ 3-1-1LED flashing/User/main.c
@@ -1,19 +1,26 @@
-#include "stm32f10x.h"// Device header
-#include "delay.h"
-int main(void){
-	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA,ENABLE);
-	GPIO_InitTypeDef P;
-	P.GPIO_Mode = GPIO_Mode_Out_PP;
-	P.GPIO_Pin = GPIO_Pin_0;
-	P.GPIO_Speed =  GPIO_Speed_50MHz;
-	GPIO_Init(GPIOA,&P);
-	while(1){
-		
-		GPIO_ResetBits(GPIOA,GPIO_Pin_0);
-		Delay_ms(500);
-		GPIO_SetBits(GPIOA,GPIO_Pin_0);
-		Delay_ms(500);
-	
-	}
-	
+#include "stm32f10x.h"
+#include "Delay.h"
+
+int main(void)
+{
+    // 1. 开启 GPIOA 时钟
+    RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA, ENABLE);
+
+    // 2. 同时初始化 PA0 和 PA1 为推挽输出
+    GPIO_InitTypeDef GPIO_InitStructure;
+    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
+    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_6 | GPIO_Pin_7;  // 两个引脚一起配
+    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+    GPIO_Init(GPIOA, &GPIO_InitStructure);
+
+    while (1)
+    {
+        // 3. 同时拉低 PA0 和 PA1
+        GPIO_ResetBits(GPIOA, GPIO_Pin_6 | GPIO_Pin_7);
+        Delay_ms(500);
+
+        // 4. 同时拉高 PA0 和 PA1
+        GPIO_SetBits(GPIOA, GPIO_Pin_6 | GPIO_Pin_7);
+        Delay_ms(500);
+    }
 }
